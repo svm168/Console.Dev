c681a157275589beff64aefa7b298f57fab22e86
svm168 <skumarjha089@gmail.com>
2026-05-28 07:08:05 +0530

    member management functionality

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -38,8 +38,8 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
                     </DropdownMenuItem>
                 )}
                 {isAdmin && (
-                    <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
-                        Manage Members
+                    <DropdownMenuItem onClick={() => onOpen("members", { server })} className="px-3 py-2 text-sm cursor-pointer">
+                        Manage Devs
                         <Users className="h-4 w-4 ml-auto"/>
                     </DropdownMenuItem>
                 )}
