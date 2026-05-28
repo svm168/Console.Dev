5dbcdd888f2d8cdf7e7eb352a225b3b024878112
svm168 <skumarjha089@gmail.com>
2026-05-28 11:39:06 +0530

    leaveServer and deleteServer functionality.

--- app/api/servers/[serverId]/route.ts
+++ app/api/servers/[serverId]/route.ts
@@ -29,3 +29,25 @@ export async function PATCH(req: Request, { params }: { params: Promise<{ server
         return new NextResponse("Internal Error", { status: 500 })
     }
 }
+
+export async function DELETE(req: Request, { params }: { params: Promise<{ serverId: string}>}) {
+    try {
+        const profile = await currentProfile();
+        if(!profile) return new NextResponse("Unauthorized", { status: 401 })
+
+        const { serverId } = await params
+        if(!serverId) return null
+
+        const server = await db.server.delete({
+            where: {
+                id: serverId,
+                profileId: profile.id,
+            },
+        })
+
+        return NextResponse.json(server)
+    } catch (error) {
+        console.log("[SERVER_ID_DELETE]", error)
+        return new NextResponse("Internal Error", { status: 500 })
+    }
+}
--- components/modals/edit-server-modal.tsx
+++ components/modals/edit-server-modal.tsx
@@ -69,9 +69,9 @@ export const EditServerModal = () => {
         <Dialog open={isModalOpen} onOpenChange={handleClose}>
             <DialogContent className="bg-white text-black p-0 overflow-hidden">
                 <DialogHeader className="pt-8 px-6">
-                    <DialogTitle className="text-2xl text-center font-bold">Create Your Server!</DialogTitle>
+                    <DialogTitle className="text-2xl text-center font-bold">Edit Server Details!</DialogTitle>
                     <DialogDescription className="text-center text-zinc-500">
-                        Realise your server with a name and an avatar. <br /> You can always change it later
+                        Customise your serveer by changing its name and avatar.
                     </DialogDescription>
                 </DialogHeader>
 
